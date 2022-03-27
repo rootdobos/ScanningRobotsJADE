322b6df4382b6511ecedcd55f3c170e572c996e6
rootdobos <49080519+rootdobos@users.noreply.github.com>
2022-03-27 16:00:09 +0300

    realized first communication
    communication between manager and agents

--- src/com/company/DataManagerAgent.java
+++ src/com/company/DataManagerAgent.java
@@ -1,5 +1,15 @@
 package com.company;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.Property;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -7,6 +17,8 @@ import org.opencv.core.Scalar;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DataManagerAgent extends Agent
@@ -14,20 +26,64 @@ public class DataManagerAgent extends Agent
     private String _EnvironmentPath;
     private Imgcodecs _ImageCodecs;
     private Mat _ScanningInformation;
+    private List<AID>  _Scanners= new ArrayList<AID>();
     protected void setup() {
         System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
         System.out.println(getLocalName()+" STARTED");
         Object[] args= getArguments();
+
         if (args != null && args.length > 0) {
             _EnvironmentPath=(String) args[0];
         }
         _ImageCodecs= new Imgcodecs();
         Mat environment= _ImageCodecs.imread(_EnvironmentPath,Imgcodecs.IMREAD_GRAYSCALE);
         System.out.println("Environment size: " +environment.size().height +":"+environment.size().width+ ":" +environment.channels());
-        _ScanningInformation= new Mat(environment.size(), CvType.CV_8UC1);
+        _ScanningInformation= new Mat(environment.size(), CvType.CV_8UC3);
         _ScanningInformation.setTo(new Scalar(255));
         System.out.println("Sinf size: " +_ScanningInformation.size().height +":"+_ScanningInformation.size().width+ ":" +_ScanningInformation.channels());
         //HighGui.imshow("Sinf",_ScanningInformation);
         //HighGui.waitKey();
+        try {
+            DFAgentDescription dfd = new DFAgentDescription();
+            dfd.setName(getAID());
+            ServiceDescription sd = new ServiceDescription();
+            sd.setName("ImageScanning");
+            sd.setType("image-scanning");
+            sd.addOntologies("image-scanning-ontology");
+            sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
+            sd.addProperties(new Property("agent", "manager"));
+            dfd.addServices(sd);
+            DFService.register(this, dfd);
+        }
+        catch (FIPAException fe){
+            fe.printStackTrace();
+        }
+        addBehaviour(new CyclicBehaviour(this) {
+            public void action() {
+                // listen if a greetings message arrives
+                ACLMessage msg = receive(MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+                if (msg != null) {
+                    String msgContent= msg.getContent();
+                    String[] msgContentParts= msgContent.split("@");
+                    if (msgContentParts[0].equalsIgnoreCase("new_scanner_checkin")) {
+                        // if a greetings message is arrived then send an ANSWER
+                        System.out.println(myAgent.getLocalName()+" RECEIVED CHECKIN MESSAGE FROM "+msg.getSender().getLocalName());
+                        _Scanners.add(msg.getSender());
+                        ACLMessage reply = msg.createReply();
+                        String message= "environment_path@"+_EnvironmentPath;
+                        reply.setContent(message);
+                        myAgent.send(reply);
+                        System.out.println(myAgent.getLocalName()+" SENT ENVIRONMENT PATH TO "+msg.getSender().getLocalName());
+                    }
+                    else {
+                        System.out.println(myAgent.getLocalName()+" Unexpected message received from "+msg.getSender().getLocalName());
+                    }
+                }
+                else {
+                    // if no message is arrived, block the behaviour
+                    block();
+                }
+            }
+        });
     }
 }
--- src/com/company/ScanningAgent.java
+++ src/com/company/ScanningAgent.java
@@ -12,13 +12,73 @@ import jade.core.Runtime;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.wrapper.*;
-
+import jade.util.leap.Iterator;
+import org.opencv.core.Point;
 public class ScanningAgent extends Agent
 {
-    protected void setup()
-    {
-        System.out.println("Hello World!");
-        System.out.println("My name is: "+ getLocalName());
+    private AID _DataManager;
+    private Point _Position;
+protected void setup(){
+    System.out.println("Agent: "+getLocalName()+" searching for manager");
+    try{
+        DFAgentDescription template= new DFAgentDescription();
+        ServiceDescription templateSd= new ServiceDescription();
+        templateSd.setType("image-scanning");
+        template.addServices(templateSd);
+
+        SearchConstraints sc = new SearchConstraints();
+        DFAgentDescription[] results = DFService.search(this, template);
+        if (results.length > 0) {
+            System.out.println("Agent "+getLocalName()+" found the manager:");
+            for (int i = 0; i < results.length; ++i) {
+                DFAgentDescription dfd = results[i];
+                AID provider = dfd.getName();
+                _DataManager= provider;
+                // The same agent may provide several services; we are only interested
+                // in the weather-forcast one
+//                Iterator it = dfd.getAllServices();
+//                while (it.hasNext()) {
+//                    ServiceDescription sd = (ServiceDescription) it.next();
+//                    if (sd.getType().equals("weather-forecast")) {
+//                        System.out.println("- Service \""+sd.getName()+"\" provided by agent "+provider.getName());
+//                    }
+//                }
+            }
+            ACLMessage msg= new ACLMessage(ACLMessage.INFORM);
+
+            msg.setContent("new_scanner_checkin");
+            msg.addReceiver(_DataManager);
+            send(msg);
+            System.out.println(getLocalName()+" SENT CHECKIN MESSAGE  TO "+_DataManager.getName());
+        }
+        else {
+            System.out.println("Agent "+getLocalName()+" did not find the Manager");
+        }
+        addBehaviour(new CyclicBehaviour(this) {
+            public void action() {
+                // listen if a greetings message arrives
+                ACLMessage msg = receive(MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+                if (msg != null) {
+                    String msgContent= msg.getContent();
+                    String[] msgContentParts= msgContent.split("@");
+                    if (msgContentParts[0].equalsIgnoreCase("environment_path")) {
+                        // if a greetings message is arrived then send an ANSWER
+                        System.out.println(myAgent.getLocalName()+" RECEIVED THE ENVIRONMENT PATH FROM "+msg.getSender().getLocalName());
+                    }
+                    else {
+                        System.out.println(myAgent.getLocalName()+" Unexpected message received from "+msg.getSender().getLocalName());
+                    }
+                }
+                else {
+                    // if no message is arrived, block the behaviour
+                    block();
+                }
+            }
+        });
     }
+    catch (FIPAException fe) {
+        fe.printStackTrace();
+    }
+}
 
 }
